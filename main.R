5b82286a57b7242583e09d8fdd22170085e77fd9
dan-burke <daniel.burke29@hotmail.com>
2024-03-15 23:19:52 +0000

    plotting code for looking at data and comments on potential model variables in txt file

--- main.R
+++ main.R
@@ -16,7 +16,10 @@ cwd <- getwd()
 load("datasets_project.RData")
 
 df <- TBdata
+
+df
 head(df)
+summary(df)
 
 #-----Visualising Data------
 plot.map(TBdata$TB[TBdata$Year==2014],n.levels=7,main="TB counts for 2014")
@@ -29,5 +32,19 @@ model1 <- gam(TB~s(Density, by=Region, k=4, bs="cs")+
               data=TBdata)
 summary(model1)
 
+par(mfrow=(c(1,2)))   # add number of subplots for number of smooth functions
 plot.gam(model1)
 
+?gam.models
+?gam
+
+
+#-----Understanding Some Relationships in the Data-----
+plot(df$Urbanisation, df$TB)   # simple 2d scatter plot to see a variable against TB
+# scatter plot with TB as colour scale to see about potential interaction terms
+ggplot(df, aes(x=Poverty, y=Density, color=TB)) +
+  geom_point() +
+  scale_color_gradient(low = "blue", high = "red", limits=c(0, 1000)) +   # set limits to stop outliers skewing the scale
+  theme_bw() + 
+  labs(color = 'TB Cases', title = 'Potential Interaction Terms')
+  
