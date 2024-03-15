7ccaaa7262ec557a5d2ee099d1d302e7c2b31cd6
r4r4r4r4r4r4r4r4r4r4r4r4r4r4 <arthurlawlessfinch@gmail.com>
2024-03-15 14:17:43 +0000

    update main

--- main.R
+++ main.R
@@ -1,13 +1,32 @@
 #MAIN FILE FOR WORKING
 
 #import dependencies
+install.packages(mgcv) #for GAM
+
 library(ggplot2)
+library(dplyr)
 library(tidyverse)
+library(fields)
+library(maps)
+library(sp)
+library(mgcv) #need for GAM
 
 #get filepath
 cwd <- getwd()
-
 load("datasets_project.RData")
 
 df <- TBdata
 head(df)
+
+#-----Visualising Data------
+plot.map(TBdata$TB[TBdata$Year==2014],n.levels=7,main="TB counts for 2014")
+
+
+#-----Making first GAM-----
+
+model1 <- gam(formula=TB~Density+Poor_Sanitation,
+              data=TBdata)
+summary(model1)
+
+plot.gam(model1)
+
