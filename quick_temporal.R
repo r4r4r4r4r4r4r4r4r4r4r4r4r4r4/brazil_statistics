a0864ca9dcd3952a9b9eb7d834ce36e071ec934c
dan-burke <daniel.burke29@hotmail.com>
2024-03-21 20:49:17 +0000

    temporal plot

--- quick_temporal.R
+++ quick_temporal.R
@@ -6,10 +6,14 @@ library(fields)
 library(maps)
 library(sp)
 library(mgcv)
+library(patchwork)
 
 # Loading DIR and DF
 setwd('/Users/matthewfound/Documents/MSc Data Science with AI/Statistical Data Modelling/Group Project')
 load('/Users/matthewfound/Documents/MSc Data Science with AI/Statistical Data Modelling/Group Project/datasets_project.RData')
+
+cwd <- getwd()
+load("datasets_project.RData")
 
 # Defining TB Risk
 df <- TBdata
@@ -23,6 +27,8 @@ model6 <- gam(TB ~ s(Timeliness, k=50, bs='cs') + s(Urbanisation, k=65, bs='cs')
                 s(Poor_Sanitation, k=65, bs='cs') +
                 offset(log(Population)), family=poisson(link='log'), data=df)
 
+TBR_preds = predict(model6, new_data=df, type='link') #this returns log of predicted cases
+df$TBR_preds = (exp(TBR_preds) / df$Population) * 100000
 
 # TEMPORAL ANALYSIS ----
 
@@ -33,13 +39,14 @@ plot.map(df$TBR[df$Year==2012], n.levels=7, main="TB Risk 2012")
 plot.map(df$TBR[df$Year==2013], n.levels=7, main="TB Risk 2013")
 plot.map(df$TBR[df$Year==2014], n.levels=7, main="TB Risk 2014")
 
+df$Year <- as.factor(df$Year)
 # Plot density of TB risk over the years
-density_plot <- ggplot(df, aes(x = TBR, fill = as.factor(Year))) +
+density_plot <- ggplot(df, aes(x = TBR, fill = Year)) +
   geom_density(alpha = 0.5) +
   labs(title = "Density Plot of TB Risk Over the Years",
        x = "TB Risk",
        y = "Density") +
-  theme_minimal()
+  theme_bw()
 print(density_plot)
 
 # Plot TBR by region colored by year as scatter plot
@@ -72,3 +79,12 @@ print(summary_stats) # RESULTS ALDO SUGGEST NO SIGNIFICANT CHANGES - FAIRLY CONS
 anova_result <- aov(TBR ~ Year, data = df)
 summary(anova_result) # P >> 0.05 - NO SIGNIFICANT DIFFERENCES IN TBR YEAR ON YEAR...
 
+box_plot <- ggplot(df, aes(x = as.factor(Year), y = TBR)) +
+  geom_boxplot() +
+  labs(title = "TB Risk Distribution by Year",
+       x = "Year",
+       y = "TB Risk") +
+  theme_bw()
+box_plot
+temporal_plot <- (box_plot + density_plot) + plot_layout(ncol = 2)
+temporal_plot
